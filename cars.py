4dbe1f1bb7319add4ac970f7f55afcf0edf78612
Holly Wilson <hollywilson1@mail.weber.edu>
2024-04-03 17:46:21 -0600

    edit

--- cars.py
+++ cars.py
@@ -20,11 +20,11 @@ cars.insert(2,'Toyota')
 # `problem 9. print the array to the console`
 print (cars)
 # `problem 10. Remove element 5 of the array (hint look at options for pop())`
-
+cars.pop(5)
 # `problem 11. print the array to the console`
 print (cars)
 # `problem 12. Sort the array in ascending order`
-cars.sort
+cars.sort()
 # `problem 13. print the array to the console`
 print (cars)
 # `problem 14. Sort the array in descending order`
@@ -32,7 +32,8 @@ cars.sort(reverse=True)
 # `problem 15. print the array to the console`
 print (cars)
 # `problem 16. create a variable called my_array_length with a value of the cars array length (spelling, capitilization, and spaces matter)`
-
+my_array_length =str( len(cars))
 # `problem 17. create a variable called array_string with a value of 'The length of my array is ' (spelling, capitilization, and spaces matter)`
-
+array_string =  'The length of my array is ' 
 # `problem 18. print array_string concatenated with my_array_length to the console.`
+print(array_string + my_array_length)
