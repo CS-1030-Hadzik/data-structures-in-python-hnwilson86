a284b13f6627aeaacc10375a9be27534d843db84
Holly Wilson <hollywilson1@mail.weber.edu>
2024-04-08 18:21:08 -0600

    person

--- Person.py
+++ Person.py
@@ -14,5 +14,19 @@ class Person:
         def birthday(self):
             self.age = self.age + 1
             print('Happy Birthday, you are now', self.age, 'years old')
+        def person_details(self):
+            print('Name:', self.name, '\nAge:', self.age)
 
-            
+
+# Instance of the class
+# Instantiate the object
+
+scott = Person('Scott', 44)
+alice = Person('Alice', 32)
+
+scott.person_details()
+alice.person_details()
+
+alice.birthday()
+
+alice.person_details()
